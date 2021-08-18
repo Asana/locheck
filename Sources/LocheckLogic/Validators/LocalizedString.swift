e22895dae3d5543e770eafde7c86b912f6375f54
Steve Landey <stevelandey@asana.com>
2021-08-18 15:58:23 -0700

    Move LocalizedString to its own file

---
+++ Sources/LocheckLogic/Validators/LocalizedString.swift
@@ -0,0 +1,90 @@
+//
+//  LocalizedString.swift
+//
+//
+//  Created by Steve Landey on 8/18/21.
+//
+
+import Files
+import Foundation
+
+protocol Filing {
+    var nameExcludingExtension: String { get }
+    var path: String { get }
+}
+
+extension File: Filing {}
+
+struct FormatArgument {
+    let specifier: String
+    let position: Int
+}
+
+private extension FormatArgument {
+    init(specifier: String, positionString: String) {
+        self.specifier = specifier
+        position = NumberFormatter().number(from: positionString)!.intValue
+    }
+}
+
+struct LocalizedString {
+    let key: String
+    let string: String
+    let keyArguments: [FormatArgument]
+    let valueArguments: [FormatArgument]
+    let file: Filing
+    let line: Int
+
+    init?(string: String, file: Filing, line: Int, problemReporter: ProblemReporter) {
+        // https://stackoverflow.com/a/37032779
+        let stringPattern = "\"[^\"\\\\]*(\\\\.[^\"\\\\]*)*\""
+        let pattern = "^(\(stringPattern)) = (\(stringPattern));$"
+        let stringLiteralRegex = try! NSRegularExpression(
+            pattern: pattern,
+            options: .anchorsMatchLines)
+        guard let strings = stringLiteralRegex
+            .matches(in: string, options: [], range: NSRange(location: 0, length: string.count))
+            .first?
+            .getGroupStrings(original: string) else {
+            return nil
+        }
+        guard strings.count == 2 else {
+            return nil
+        }
+        let key = String(strings[0].dropFirst().dropLast())
+        let value = String(strings[1].dropFirst().dropLast())
+        self.key = key
+        self.string = string
+        self.file = file
+        self.line = line
+        keyArguments = LocalizedString.parseArguments(string: key, problemReporter: problemReporter)
+        valueArguments = LocalizedString.parseArguments(string: value, problemReporter: problemReporter)
+    }
+
+    static func parseArguments(string: String, problemReporter: ProblemReporter) -> [FormatArgument] {
+        if string.contains("$") {
+            return try! NSRegularExpression(pattern: "%(\\d+)\\$([@a-z]+)", options: [])
+                .matches(in: string, options: [], range: NSRange(location: 0, length: string.count))
+                .compactMap { (match: NSTextCheckingResult) -> FormatArgument? in
+                    let groupStrings = match.getGroupStrings(original: string)
+                    return FormatArgument(
+                        specifier: groupStrings[2],
+                        positionString: groupStrings[1])
+                }
+        } else {
+            return try! NSRegularExpression(pattern: "%([@a-z]+)", options: [])
+                .matches(in: string, options: [], range: NSRange(location: 0, length: string.count))
+                .enumerated()
+                .compactMap { (i: Int, match: NSTextCheckingResult) -> FormatArgument? in
+                    let groupStrings = match.getGroupStrings(original: string)
+                    guard !groupStrings.isEmpty else {
+                        problemReporter.logInfo("XXX \(string.debugDescription) \(groupStrings.debugDescription)")
+                        return nil
+                    }
+                    return FormatArgument(
+                        specifier: groupStrings.last!,
+                        position: i + 1)
+                }
+        }
+    }
+}
