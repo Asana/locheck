ca3083e3997148f0a62b07f9ff5497bc9d99c1cb
Steve Landey <stevelandey@asana.com>
2021-08-25 17:07:23 -0700

    Detect missing strings

--- Sources/LocheckLogic/Validators/validateStringsdict.swift
+++ Sources/LocheckLogic/Validators/validateStringsdict.swift
@@ -9,16 +9,38 @@ import Files
 import Foundation
 
 public func validateStringsdict(
-    base: File,
-    translation: File,
+    base baseFile: File,
+    translation translationFile: File,
     translationLanguageName: String,
     problemReporter: ProblemReporter) {
-    guard let baseStringsdict = Stringsdict(path: base.path, problemReporter: problemReporter) else {
+    guard let base = Stringsdict(path: baseFile.path, problemReporter: problemReporter) else {
         return
     }
-    print("=======")
-    guard let translationStringsdict = Stringsdict(path: translation.path, problemReporter: problemReporter) else {
+    guard let translation = Stringsdict(path: translationFile.path, problemReporter: problemReporter) else {
         return
+    }
+
+    let baseKeys = Set(base.entries.map(\.key))
+    let translationKeys = Set(translation.entries.map(\.key))
+
+    for key in baseKeys {
+        if !translationKeys.contains(key) {
+            problemReporter.report(
+                .error,
+                path: baseFile.path,
+                lineNumber: 0,
+                message: "Key '\(key)' is missing from \(translationLanguageName)")
+        }
+    }
+
+    for key in translationKeys {
+        if !baseKeys.contains(key) {
+            problemReporter.report(
+                .error,
+                path: translationFile.path,
+                lineNumber: 0,
+                message: "Key '\(key)' is missing from the base localization")
+        }
     }
 //    print(baseStringsdict)
 //    print(translationStringsdict)
