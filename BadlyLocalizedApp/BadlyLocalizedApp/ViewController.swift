745eb95b9a46f82f5f870fbc9120aff42ef610ae
Steve Landey <stevelandey@asana.com>
2021-09-01 16:14:01 -0700

    Add an Xcode project proving that the errors we catch are real

---
+++ BadlyLocalizedApp/BadlyLocalizedApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BadlyLocalizedApp
+//
+//  Created by Steve Landey on 9/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        printLocalizationExamples()
+    }
+
+
+}
+
